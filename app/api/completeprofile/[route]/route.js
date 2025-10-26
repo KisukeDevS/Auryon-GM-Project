71746cbdf99c295403a4aed548bb92b96745d76f
KisukeDevS <hzurriyat@gmail.com>
2025-10-26 16:18:43 +0500

    Update route.js

--- app/api/completeprofile/[route]/route.js
+++ app/api/completeprofile/[route]/route.js
@@ -176,7 +176,7 @@ export async function GET(request, context) {
     } else {
       console.log(session.user.email , "iiam gmaiii")
       forms = await FormSch.findOne({
-      email: `${session.user.email}`,
+      userName: `${newToken?session.user.userName:session.user.name}`,
       });
       if (forms) {
         return NextResponse.json({ success: true, res: forms });
